c4d16e7b89f1c305ed4531d4cd674c60ca186fa7
PRABHAHARAN A <122508061+PRABHAHARAN-2020@users.noreply.github.com>
2025-02-18 07:18:25 +0000

    cmt

--- ArrayEnQueue.java
+++ ArrayEnQueue.java
@@ -16,17 +16,30 @@ public void enQueue(int data)
 {
 
 // Type your code here
+if (isFull())
+{
+System.out.println("Queue is Overflow. Not possible to insert in Full Queue");
+display();
+}
+else
+{
+queue[++rear] = data;
+System.out.println("Element is inserted");
+}
+
 
 }
 public boolean isFull()
 {
 
 // Type your code here
+    return (size()==capacity);
 }
 public boolean isEmpty()
 {
 
 // Type your code here
+return (front==rear);
 }
 
 
@@ -52,6 +65,17 @@ int data;
     System.out.println("Enter the number of elements to be inserted:");
 
 // Type your code here
-
-
-} 
+    n=sc.nextInt();
+    ArrayEnQueue q=new ArrayEnQueue(n);
+    do
+    {
+System.out.println("Enter the element to insert:");
+        data=sc.nextInt();
+q.enQueue(data);
+System.out.println("Do you want to insert(y/n):");
+ch=sc.next().charAt(0);
+}
+while(ch!='n');
+q.display();
+}
+}
